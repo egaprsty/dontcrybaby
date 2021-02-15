a157d05210be965066921830e3e1982d160a6cb9
Ega Prasetya <54002683+egaprsty@users.noreply.github.com>
2021-02-15 08:17:53 +0700

    Update esde.js

--- pages/esde.js
+++ pages/esde.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import {NextSeo} from 'next-seo';
 
 export const getServerSideProps = async () => {
-    const res = await fetch('https://dontcrybaby.vercel.app/api/dataSd.js')
+    const res = await fetch('https://egaprsty.vercel.app/api/dataSd.js')
     const data = await res.json();
 
     return{
