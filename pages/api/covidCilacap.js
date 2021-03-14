3c28808de82438764c07f3d84742907866d1f805
Ega Prasetya <54002683+egaprsty@users.noreply.github.com>
2021-03-14 16:44:14 +0700

    Update covidCilacap.js

--- pages/api/covidCilacap.js
+++ pages/api/covidCilacap.js
@@ -12,7 +12,7 @@ export default (req, res) => {
             name: 'Kec. Wanareja',
             kontak: 0,
             dirawat: 0,
-            positif: 3
+            positif: 4
         },
         {
             name: 'Kec. Majenang',
@@ -23,8 +23,8 @@ export default (req, res) => {
         {
             name: 'Kec. Cimanggu',
             kontak: 11,
-            dirawat: 3,
-            positif: 5
+            dirawat: 2,
+            positif: 1
         },
         {
             name: 'Kec. Karangpucung',
@@ -36,19 +36,19 @@ export default (req, res) => {
             name: 'Kec. Cipari',
             kontak: 3,
             dirawat: 0,
-            positif: 6
+            positif: 1
         },
         {
             name: 'Kec. Sidareja',
             kontak: 5,
-            dirawat: 3,
-            positif: 4
+            dirawat: 2,
+            positif: 5
         },
         {
             name: 'Kec. Kedungreja',
             kontak: 0,
             dirawat: 2,
-            positif: 5
+            positif: 6
         },
         {
             name: 'Kec. Patimuan',
@@ -65,8 +65,8 @@ export default (req, res) => {
         {
             name: 'Kec. Bantarsari',
             kontak: 0,
-            dirawat: 3,
-            positif: 6
+            dirawat: 2,
+            positif: 5
         },
         {
             name: 'Kec. Kawunganten',
@@ -78,13 +78,13 @@ export default (req, res) => {
             name: 'Kec. Jeruklegi',
             kontak: 4,
             dirawat: 1,
-            positif: 42
+            positif: 29
         },
         {
             name: 'Kec. Kesugihan',
             kontak: 6,
             dirawat: 1,
-            positif: 30
+            positif: 28
         },
         {
             name: 'Kec. Adipala',
@@ -96,13 +96,13 @@ export default (req, res) => {
             name: 'Kec. Maos',
             kontak: 0,
             dirawat: 0,
-            positif: 8
+            positif: 7
         },
         {
             name: 'Kec. Kroya',
             kontak: 8,
             dirawat: 0,
-            positif: 21
+            positif: 19
         },
         {
             name: 'Kec. Binangun',
@@ -114,7 +114,7 @@ export default (req, res) => {
             name: 'Kec. Sampang',
             kontak: 0,
             dirawat: 0,
-            positif: 2
+            positif: 1
         },
         {
             name: 'Kec. Nusawungu',
@@ -132,13 +132,13 @@ export default (req, res) => {
             name: 'Kec. Cilacap Selatan',
             kontak: 14,
             dirawat: 1,
-            positif: 82
+            positif: 90
         },
         {
             name: 'Kec. Cilacap Tengah',
             kontak: 33,
-            dirawat: 6,
-            positif: 65
+            dirawat: 5,
+            positif: 70
         },
         {
             name: 'Kec. Kampung Laut',
